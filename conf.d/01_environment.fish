4bd2f34e36b164b84b505d105b99a0a77c1dcde9
kielmarj <kielmarj@gmail.com>
2024-10-21 14:07:16 -0400

    .

--- conf.d/01_environment.fish
+++ conf.d/01_environment.fish
@@ -4,9 +4,8 @@ set -Ux CARGO_HOME /home/jess/.local/lib/cargo
 set -Ux COLUMNS 80
 set -Ux EDITOR micro
 set -Ux GNUPGHOME /home/jess/.local/share/gpg
-set -Ux --unpath GOPATH /home/jess/.local/lib/go
+set -Ux GOPATH /home/jess/.local/lib/go
 set -Ux NPM_CONFIG_USERCONFIG /home/jess/.config/npm/npmrc
-set -Ux OPENER mimeo
 set -Ux PAGER bat
 set -Ux PARALLEL_HOME /home/jess/.config/parallel
 set -Ux --unpath RIPGREP_CONFIG_PATH /home/jess/.config/ripgrep/ripgreprc
@@ -15,4 +14,4 @@ set -Ux SHELL /usr/bin/fish
 set -Ux VISUAL code
 set -U fish_features qmark-noglob
 set -U fish_key_bindings fish_default_key_bindings
-set -U fish_user_paths /home/jess/Projects/exercism-solutions/bash /home/jess/Projects/testing /home/jess/Projects/script-collection /home/jess/bin
+set -U fish_user_paths $HOME/bin $HOME/Projects/testing $CARGO_HOME/bin $HOME/.local/lib/npm/bin
