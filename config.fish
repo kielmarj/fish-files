7d3f5a513d4b19db4e2df2cf1e7b98376858c602
Jess Kielmar <Naturalborncyborg@proton.me>
2025-03-03 10:24:44 -0500

    add describe_functions to configh.fish

--- config.fish
+++ config.fish
@@ -3,13 +3,27 @@ status is-interactive || exit
 # Load theme
 fish_config theme choose uwunicorn
 
-# CTRL + F = wikiman
-source /usr/share/wikiman/widgets/widget.fish
-
-# Prints a random Zen quote from zenquotes.io before fish_greeting
+# Random Zen quote from zenquotes.io before fish_greeting - prints before fish_greeting
 echo ""
 curl -s -X GET -H "Content-Type: application/json" https://zenquotes.io/api/random | \
 jq -r '" " as $space | .[0] | .q + "\n\n\($space * ((.q |length)-(.a |length) - 2))--" + .a' |  \
 tr -s \[:blank:\] | \
 lolcat
 echo ""
+
+# CTRL + F = wikiman
+source /usr/share/wikiman/widgets/widget.fish
+
+# Load function descriptions
+function describe_functions --description "Load function descriptions for tab completions"
+    set -l output ""
+    for dir in $HOME/.config/fish/functions /usr/share/fish/functions
+        for file in $dir/*.fish
+            set -l cmd (basename $file .fish)
+            set -l desc (functions -Dv $cmd)[5]
+            set -a output "- `$cmd`: $desc"
+        end
+    end
+    printf '%s\n' $output | sort
+end
+describe_functions 2&> /dev/null
--- functions/bangbang.fish
+++ functions/bangbang.fish
@@ -1,4 +1,4 @@
-function bangbang --description 'fn Add last command to current commandline with !!'
-        # Use with abbreviation: abbr -a --position anywhere --function bangbang -- !!
+function bangbang --description 'fn Insert last cmd with !!'
+        # Use with abbreviation: 'abbr -a --position anywhere --function bangbang -- !!'
         printf %s $history[1]
 end
